35f9840e9941243a9bb03841d92900177ccc7a42
Yejin Kim <mumuandsander@gmail.com>
2022-08-24 16:42:15 +0900

    refactoring code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function fetchPage(name){
     fetch('./data/'+name).then(function(response) {
         response.text().then(function(text) {
-            document.querySelector('#동').innerHTML = text;
+            document.querySelector('.articleText').innerHTML = text;
         })
     })
 }
@@ -12,8 +12,69 @@ if(location.hash){
     fetchPage('welcome')
 }
 
+
 fetch('./data/list_동').then(function(response) {
     response.text().then(function(text) {
-        document.querySelector('#navBar1').innerHTML = text;
+        let items = text.split(',');
+        let i = 0;
+        let tags = '';
+        while (i < items.length) {
+            let item = items[i];
+            item = item.trim();
+            let tag = `<li><a href="#!${item}" onclick="fetchPage('${item}')">${item}</a></li>`;
+            tags = tags + tag;
+            i = i + 1;
+        }
+        document.querySelector('#navBar1').innerHTML = tags;
     })
-})
+});
+
+
+// 목록
+fetch('./data/list_서').then(function(response) {
+    response.text().then(function(text) {
+        let items = text.split(',');
+        let i = 0;
+        let tags = '';
+        while (i < items.length) {
+            let item = items[i];
+            item = item.trim();
+            let tag = `<li><a href="#!${item}" onclick="fetchPage('${item}')">${item}</a></li>`;
+            tags = tags + tag;
+            i = i + 1;
+        }
+        document.querySelector('#navBar2').innerHTML = tags;
+    })
+});
+
+fetch('./data/list_남').then(function(response) {
+    response.text().then(function(text) {
+        let items = text.split(',');
+        let i = 0;
+        let tags = '';
+        while (i < items.length) {
+            let item = items[i];
+            item = item.trim();
+            let tag = `<li><a href="#!${item}" onclick="fetchPage('${item}')">${item}</a></li>`;
+            tags = tags + tag;
+            i = i + 1;
+        }
+        document.querySelector('#navBar3').innerHTML = tags;
+    })
+});
+
+fetch('./data/list_북').then(function(response) {
+    response.text().then(function(text) {
+        let items = text.split(',');
+        let i = 0;
+        let tags = '';
+        while (i < items.length) {
+            let item = items[i];
+            item = item.trim();
+            let tag = `<li><a href="#!${item}" onclick="fetchPage('${item}')">${item}</a></li>`;
+            tags = tags + tag;
+            i = i + 1;
+        }
+        document.querySelector('#navBar4').innerHTML = tags;
+    })
+});
